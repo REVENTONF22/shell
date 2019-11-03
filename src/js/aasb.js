3435aaedffed398ce724660dd5466f915b131c4e
mu2306 <185471581@qq.com>
2019-11-04 02:05:04 +0800

    'aasb'

---
+++ src/js/aasb.js
@@ -0,0 +1,25 @@
+﻿
+// export default {
+// 	a: 10,
+
+// 	b: 20,
+
+// 	c: 30,
+
+// 	d: 40
+// }
+
+// export let w = 100;
+
+// export let x = 200;
+
+// export let y = 300;
+
+// export let z = 400;
+
+
+export let num = '你大爷的';
+
+export let getSum = function (n) {
+	console.log(n);
+}
--- src/js/test.js
+++ src/js/test.js
@@ -1,6 +1,13 @@
 ﻿// 测试模拟 React 的创建引入
-import React, { Component } from './react';
+// import React, { Component } from './react';
 
-console.log(React);
+// console.log(React);
 
-console.log(Component);
+// console.log(Component);
+
+
+// 测试 import a as b from 'abc' 的使用
+
+// import {num as sum} from './aasb';
+
+// console.log(sum);
